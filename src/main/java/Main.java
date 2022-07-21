effc1ed8b32630126004fa06d5fb93bc5dc5df28
Adrian Khaskin <haskin.dev@gmail.com>
2022-07-21 11:58:53 -0400

    finished implementation

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,13 +3,25 @@ import java.util.Map;
 import java.util.Optional;
 import java.util.Scanner;
 
+/*
+ *  Problems:
+ *  // This is impossible to do without changing Book class which we are not allowed to do
+ *  - print the book name along with its title if the book is present in the HashMap. 
+ * 
+ *   x Print the book’s
+ *   name and its author (separated by `written by`) if the author is present and the
+ *  “Unknown” string otherwise.
+ */
 public class Main {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        // Scanner scanner = new Scanner(System.in);
 
-        String bookName = scanner.nextLine();
+        // String bookName = scanner.nextLine();
+        String bookName = "";
         Optional<String> optAuthor = Book.getAuthor(bookName);
+        optAuthor.ifPresentOrElse(author -> System.out.println(bookName + " written by " + author),
+                () -> System.out.println("“Unknown”"));
 
         // write your code here
     }
